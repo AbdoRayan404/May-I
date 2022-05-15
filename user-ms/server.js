39bf8b12c995ae626dde3335a8fbeb60e11f9903
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-15 17:10:54 +0200

    POST register API route

--- user-ms/server.js
+++ user-ms/server.js
@@ -30,4 +30,40 @@ app.get('/api/record', (req,res)=>{
     })
 })
 
+app.post('/api/register', (req,res)=>{
+    let {username, password, public_key} = req.body;
+    let uuid = uuidv4();
+
+    //INPUT CHECK
+    if(username.length > 36 || username.length < 4){
+        res.status(401).json({'error':'username is too short or too long'})
+        return
+    }
+    if(password.length > 32 || password.length < 8){
+        res.status(401).json({'error':'password is too short or too long.'})
+        return
+    }
+    if(!public_key){
+        res.status(401).json({'error':'you should provide a public_key'})
+        return
+    }
+
+    pool.query(`INSERT INTO users(username, password, UUID, public_key) VALUES('${username}', '${password}', '${uuid}', '${public_key}')`, (err, data)=>{
+        if(err){
+            res.status(401).json({'error':err.detail})
+        }
+        else{
+            pool.query(`SELECT username, public_key FROM users WHERE username = '${username}'`, (error, saved)=>{
+                if(error){
+                    console.log(error)
+                    res.status(500).json({'error':"your data was inserted but couldn't reterive it."})
+                }else{
+                    res.status(200).json(saved.rows)
+                }
+            })
+        }
+    })
+})
+
+
 app.listen(3000)
