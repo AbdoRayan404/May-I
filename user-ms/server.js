5957201df653f332581d7861e248f68ffb0925fc
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-16 10:51:47 +0200

    importing refactored routes

--- user-ms/server.js
+++ user-ms/server.js
@@ -6,7 +6,16 @@ const app = express();
 // microservice configurations
 app.use(express.json());
 
+// exports
+const {logger, dbLogger, errHandle} = require('./routes/middlewares');
+const {public_record, register, login} = require('./routes/api')
 
+//routes
+app.use(logger)
+app.use('/api/record', public_record, dbLogger)
+app.use('/api/register', register, dbLogger)
+app.use('/api/login', login, dbLogger)
+app.use(errHandle)
 
 
 app.listen(3000)
