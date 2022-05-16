24cdaf0270eee90f2c310f0842b374e5dc30b3b4
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-16 09:21:30 +0200

    database logger middleware

--- user-ms/routes/middlewares.js
+++ user-ms/routes/middlewares.js
@@ -5,8 +5,14 @@ async function reqLogger(req, res, next){
 async function errorHandle(error, req, res, next){
     res.status(error.status).json({'error':error.msg})
 }
+async function databaseLogger(data, req, res, next){
+    if(data.method == 'error') next(data)
+
+    console.log(`[INFO] ${data.method} ${data.coulmns} from table ${data.table} limited by ${data.limit}`)
+}
 
 module.exports = {
     logger: reqLogger, 
+    dbLogger: databaseLogger,
     errHandle: errorHandle
 };
