28e812487669d73b6f0830b32da333cf1ba85e43
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-16 09:22:51 +0200

    setting imports and exports

--- user-ms/Model/database.js
+++ user-ms/Model/database.js
@@ -1,10 +1,13 @@
 const Pool = require('pg').Pool;
+const {user, host, database, password, port} = require('../env');
 
 //Database connection configuration
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    //password: process.env.PASSWORD, //uncomment if you have password in your database
-    port: Number.parseInt(process.env.PORT)
+    user: user,
+    host: host,
+    database: database,
+    //password: password, //uncomment if you have password in your database
+    port: Number.parseInt(port)
 })
+
+module.exports = pool
