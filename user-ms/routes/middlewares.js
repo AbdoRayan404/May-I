f938361e6d2a65cb42e514c705937688677ee7c0
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-16 10:10:17 +0200

    adding SELECT and INSERT logger to db logger

--- user-ms/routes/middlewares.js
+++ user-ms/routes/middlewares.js
@@ -8,7 +8,13 @@ async function errorHandle(error, req, res, next){
 async function databaseLogger(data, req, res, next){
     if(data.method == 'error') next(data)
 
-    console.log(`[INFO] ${data.method} ${data.coulmns} from table ${data.table} limited by ${data.limit}`)
+    if(data.method == 'SELECT'){
+        console.log(`[INFO] ${data.method} ${data.coulmns} from table ${data.table} limited by ${data.limit}`)
+    }
+    else if(data.method == 'INSERT'){
+        console.log(`[INFO] ${data.method} into ${data.table} table where coulmns (${data.coulmns}) is (${data.values})`)
+    }
+    
 }
 
 module.exports = {
