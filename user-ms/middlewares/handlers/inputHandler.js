b40b200f36e6eacf00c37f6cf0343a831fb5b229
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-19 14:18:50 +0200

    moving input check to middleware on its own

---
+++ user-ms/middlewares/handlers/inputHandler.js
@@ -0,0 +1,26 @@
+async function check(req, res, next){
+    
+    let {username, password, public_key} = req.body
+    
+    if(!username.match(/^[A-z]{4,36}$/g)) next({
+        method: 'error',
+        status: 401,
+        msg:'username should be between 4-36 A-Z'
+    })
+
+    if(!password.match(/^.{8,32}$/gi)) next({
+        method: 'error',
+        status: 401,
+        msg: 'password should be between 8-32'
+    })
+
+    if(public_key.length > 145) next({
+        method: 'error',
+        status: 401,
+        msg: 'public key cannot be more than 145 chars'
+    })
+
+    next()
+}
+
+module.exports = check
