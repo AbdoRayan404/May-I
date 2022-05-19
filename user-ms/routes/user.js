f75cd027e3d8cab257002b7f45a7a92fb775f00a
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-19 14:20:40 +0200

    importing input check middleware

--- user-ms/routes/user.js
+++ user-ms/routes/user.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const {login, register, update} = require('../controllers/user');
-
+const inputCheck = require('../middlewares/handlers/inputHandler');
 
 router
     .post('/login', login)
-    .post('/register', register)
+    .post('/register', inputCheck, register)
     .put('/update', update)
 
 module.exports = router
