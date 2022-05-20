effb5015dc3230d5673877dc08c6b01bd207aab4
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-20 18:53:21 +0200

    changing database PORT to DBPORT and adding PORT export

--- user-ms/config/env.js
+++ user-ms/config/env.js
@@ -1,3 +1,3 @@
 require('dotenv').config();
 
-module.exports = {USER, HOST, DATABASE, PASSWORD, PORT, SALT} = process.env;
+module.exports = {USER, HOST, DATABASE, PASSWORD, DBPORT, PORT, SALT} = process.env;
