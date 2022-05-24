0d917d06dc883e20cab22c66067ac8645f011e3a
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-24 08:50:41 +0200

    cleaning server file

--- message-ms/server.js
+++ message-ms/server.js
@@ -1,34 +1,11 @@
 const WebSocket = require('ws');
-const pool = require('./model/database');
 
-const wss = new WebSocket.WebSocketServer({ port:4000 },()=> console.log('server is up on port 4000'))
-const sockets = {
-    
-}
+//config
+const { PORT } = require('./config/env')
 
-wss.on('connection', (ws)=>{
-    ws.send(JSON.stringify({"type":"authenticate","message":"login"}))
-    sockets[ws] = {verified: false, messages: 0}
+const wss = new WebSocket.WebSocketServer({ port:PORT },()=> console.log(`server is up on port ${ PORT }`))
 
-    ws.on('message', (data)=>{
-        try{
-            data = JSON.parse(data.toString()) // buffer -> string -> json
-        }catch(err){
-            ws.send(JSON.stringify({"type":"connection", "connection":"terminate", "message":"invalid JSON."}))
-            ws.terminate()
-        }
-        
+module.exports = wss;
 
-        if(sockets[ws].verified == false){
-            if(sockets[ws].messages >= 3){
-                ws.send(JSON.stringify({"type":"connection", "connection":"terminate", "message":"did not verify after 3 requests. TERMINATING"}))
-                ws.terminate()
-            }
-            sockets[ws].messages++;
-
-            if(data.type == "login"){
-                ws.send('will continue later !!')
-            }
-        }
-    })
-})
+//loading
+const handler = require('./sockets/handler')
