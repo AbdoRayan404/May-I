09d8837b9cd79493ea422cfd84c962ea373bbe91
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-05-31 12:38:44 +0200

    adding verified function, refactor to work with array

--- message-ms/sockets/handler.js
+++ message-ms/sockets/handler.js
@@ -1,27 +1,30 @@
 const wss = require('../server')
-const sockets = require('../model/sockets')
+const {sockets, verifiedSockets} = require('../model/sockets')
 const verify = require('./unverified')
+const verified = require("./verified")
 
 wss.on("connection", async (ws)=>{
     ws.send(JSON.stringify({"type":"authenticate","message":"login"}))
-    sockets[ws] = {verified: false, messages: 0}
+    ws.verified = false
+    sockets.push(ws)
     
 
     ws.on('message', (data)=>{
-        //Data convertion
-        try{
-            data = JSON.parse(data.toString()) // buffer -> string -> json
-        }catch(err){
-            ws.send(JSON.stringify({"type":"connection", "connection":"terminate", "message":"invalid JSON."}))
-            ws.terminate()
-        }
+        console.log(ws)
         
-        if(sockets[ws].verified == false){
-            verify(ws, data)
+        try{
+            data = JSON.parse(data.toString())
+        }catch(err){
+            ws.send(JSON.stringify({"type":"error","connection":"terminate","message":"Invalid JSON."}))
+            ws.terminate();
+            return
         }
 
-
-        sockets[ws].messages++;
+        if(ws.verified == false){
+            verify(ws, data)
+        }else if(ws.verified == true){
+            verified(ws, data)
+        }
     })
 
     
