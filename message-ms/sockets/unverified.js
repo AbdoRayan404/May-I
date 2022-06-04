398e582042b78ae8ab6ea5a4b6bb923175ef6bb6
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-06-04 13:50:28 +0200

    adding type check

--- message-ms/sockets/unverified.js
+++ message-ms/sockets/unverified.js
@@ -8,32 +8,34 @@ async function verify(ws, data) {
         return
     }
 
-    let { address, password } = data;
+    if(data.type == "login"){
+        let { address, password } = data;
 
-    //HASHING and SALT reterival
-    try{
-        const SALTdata = await pool.query(`SELECT salt FROM users WHERE address = '${address}'`)
-        if(SALTdata.rowCount != 1) return ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"address is wrong."}))
-
-        password = await bcrypt.hash(password, SALTdata.rows[0].salt)
-    }catch(err){
-        ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"there was an error hashing your password."}))
-    }
-
-    //Logging in and verifing credintials
-    try{
-        const DBdata = await pool.query(`SELECT password = '${password}' FROM users  WHERE address = '${address}'`)
-
-        if(DBdata.rows[0]['?column?'] == false){
-            ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"password is wrong."}))
+        //HASHING and SALT reterival
+        try{
+            const SALTdata = await pool.query(`SELECT salt FROM users WHERE address = '${address}'`)
+            if(SALTdata.rowCount != 1) return ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"address is wrong."}))
+    
+            password = await bcrypt.hash(password, SALTdata.rows[0].salt)
+        }catch(err){
+            ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"there was an error hashing your password."}))
         }
-        else if(DBdata.rows[0]['?column?'] == true){
-            ws.send(JSON.stringify({"type":"authenticate", "authentication":"success"}))
-            ws.verified = true;
-            ws.address = address;
+    
+        //Logging in and verifing credintials
+        try{
+            const DBdata = await pool.query(`SELECT password = '${password}' FROM users  WHERE address = '${address}'`)
+    
+            if(DBdata.rows[0]['?column?'] == false){
+                ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"password is wrong."}))
+            }
+            else if(DBdata.rows[0]['?column?'] == true){
+                ws.send(JSON.stringify({"type":"authenticate", "authentication":"success"}))
+                ws.verified = true;
+                ws.address = address;
+            }
+        }catch(err){
+            ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"there was an error checking your credintials."}))
         }
-    }catch(err){
-        ws.send(JSON.stringify({"type":"authenticate", "authentication":"failed", "message":"there was an error checking your credintials."}))
     }
 }
 
