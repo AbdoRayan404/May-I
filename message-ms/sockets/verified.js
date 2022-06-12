e9d99c4b562c24631a3c315cae4a1d6de423e1b6
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-06-12 21:01:11 +0200

    check if user exists in DB, check if user connected, message sending

--- message-ms/sockets/verified.js
+++ message-ms/sockets/verified.js
@@ -1,8 +1,17 @@
 const {sockets} = require("../model/sockets")
+const pool = require('../model/database')
+const { createPending } = require('../model/mongoController')
 
 async function verified(ws, data){
     if(data.type == "message"){
         let userToSend = {}
+
+        const findUser = await pool.query(`SELECT address FROM users WHERE address = '${data.address}'`)
+
+        if(findUser.rowCount == 0){
+            ws.send(JSON.stringify({"type":"send","status":"error","reason":"this user doesn't exist"}))
+            return
+        }
 
         for(let i = 0; i < sockets.length; i++){
             if(sockets[i].address == data.address && sockets[i].verified == true){
@@ -10,7 +19,13 @@ async function verified(ws, data){
             }
         }
 
-        userToSend.send(JSON.stringify({"type":"message","message":data.message,"from": ws.address}))
+        if(userToSend = {}){ //if the user is not connected
+            createPending(data.message, ws.address, data.address)
+        }else{ //user is connected
+            userToSend.send(JSON.stringify({"type":"message","message":data.message,"from": ws.address}))
+        }
+
+        ws.send(JSON.stringify({"type":"send","status":"success","reason":"message was sent successfuly"}))
     }
 }
 
