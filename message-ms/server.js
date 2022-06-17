9758c8a6d424c0b607954c5a833a6ad6f1b684dc
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-06-17 21:33:43 +0200

    [ref] moving handler to main server.js

--- message-ms/server.js
+++ message-ms/server.js
@@ -3,9 +3,36 @@ const WebSocket = require('ws');
 //config
 const { PORT } = require('./config/env')
 
-const wss = new WebSocket.WebSocketServer({ port:PORT },()=> console.log(`server is up on port ${ PORT }`))
+//exports
+const verify = require('./handlers/unverified')
+const verified = require("./handlers/verified")
+const checkin = require("./handlers/checkin")
+const {sockets} = require('./model/sockets')
 
-module.exports = wss;
+//initial WebSockerServer
+const MayIWebSocket = require('./classes/websocket')
+const wss = new WebSocket.WebSocketServer({ port:PORT, "WebSocket":MayIWebSocket },()=> console.log(`server is up on port ${ PORT }`))
 
-//loading
-const handler = require('./sockets/handler')
+wss.on('connection', async (ws)=>{
+    ws.send(JSON.stringify({"type":"authenticate", "message":"login"}))
+    sockets.push(ws)
+
+    ws.on("message", (data)=>{
+        try{
+            data = JSON.parse(data.toString());
+        }catch(err){
+            ws.send(JSON.stringify({"type":"conection", "connection":"terminate", "reason":"Invalid JSON"}))
+            ws.terminate();
+
+            return
+        }
+
+        if(ws.verified == false){
+            verify(ws, data)
+        }else if(ws.verified == true){
+            verified(ws, data)
+        }
+    })
+})
+
+module.exports = wss
