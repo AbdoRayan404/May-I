f1652f33c1f81c94840cf4cb97e2aac5ec028b39
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-06-20 23:21:59 +0200

    changing USER -> DBUSER, disabling rejectUnauthorized

--- user-ms/Model/database.js
+++ user-ms/Model/database.js
@@ -1,13 +1,14 @@
 const Pool = require('pg').Pool;
-const {USER, HOST, DATABASE, PASSWORD, DBPORT} = require('../config/env');
+const {DBUSER, HOST, DATABASE, PASSWORD, DBPORT} = require('../config/env');
 
 //Database connection configuration
 const pool = new Pool({
-    user: USER,
+    user: DBUSER,
     host: HOST,
     database: DATABASE,
     password: PASSWORD,
-    port: Number.parseInt(DBPORT)
+    port: Number.parseInt(DBPORT),
+    ssl: {rejectUnauthorized: false}
 })
 
 module.exports = pool
--- user-ms/config/env.js
+++ user-ms/config/env.js
@@ -1,3 +1,3 @@
 require('dotenv').config();
 
-module.exports = {USER, HOST, DATABASE, PASSWORD, DBPORT, PORT, SALT} = process.env;
+module.exports = {DBUSER, HOST, DATABASE, PASSWORD, DBPORT, PORT, SALT} = process.env;
