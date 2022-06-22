e3570ba911a1350e8e79de1cc3142531cf0c5c0d
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-06-22 19:34:48 +0200

    [REF] new .env structure

--- message-ms/config/env.js
+++ message-ms/config/env.js
@@ -1,3 +1,3 @@
 require('dotenv').config();
 
-module.exports = {PORT, DBUSER, HOST, DATABASE, DBPORT, PASSWORD, MONGO_URI} = process.env
+module.exports = {PORT, POSTGRES_USER, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DATABASE_NAME, POSTGRES_PASSWORD, MONGO_URI} = process.env
--- user-ms/config/env.js
+++ user-ms/config/env.js
@@ -1,3 +1,3 @@
 require('dotenv').config();
 
-module.exports = {DBUSER, HOST, DATABASE, PASSWORD, DBPORT, PORT, SALT} = process.env;
+module.exports = {PORT, POSTGRES_USER, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_PASSWORD, POSTGRES_DATABASE_NAME, SALT} = process.env;
