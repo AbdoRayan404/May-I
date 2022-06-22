b14a31156e91320814cf8d0f556f8740ae332293
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-06-22 20:21:54 +0200

    [FIX] .address -> .ACCaddress

--- message-ms/handlers/checkin.js
+++ message-ms/handlers/checkin.js
@@ -1,7 +1,7 @@
 const { getPendings } = require('../model/mongoController')
 
 async function checkIn(ws){
-    const pendings = await getPendings(ws.address);
+    const pendings = await getPendings(ws.ACCaddress);
 
     for(let i = 0; i < pendings.length; i++){
         ws.send(JSON.stringify({"type":"pending-message","from":pendings[i]['from'], "message":pendings[i]['message'], "sent_at": new Date(pendings[i]['sent_at']).toDateString() }))
