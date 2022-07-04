f2b37977685936e1fa671eac12edd19b01742ecd
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-04 09:40:34 +0200

    [FEATURE] restoring messages on login

--- user-ms/controllers/user/login.js
+++ user-ms/controllers/user/login.js
@@ -1,11 +1,12 @@
 const pool = require('../../model/database')
+const {getAllMessages} = require('../../model/mongoController')
 
 async function login(req, res, next){
     let {address} = req.body;
 
     let query = {
         method: 'SELECT',
-        columns: `address, username, public_key`,
+        columns: `address, username, public_key, store_messages`,
         table: 'users',
         condition: 'WHERE',
         conditionKey: 'address',
@@ -14,8 +15,12 @@ async function login(req, res, next){
 
     try{
         const userData = await pool.query(`${query.method} ${query.columns} FROM ${query.table} WHERE ${query.conditionKey} = ${query.conditionValue}`)
+        let userMessages = {}
+        if(userData.rows[0]['store_messages'] == true){
+            userMessages = await getAllMessages(address)
+        }
 
-        res.status(200).json({'address': userData.rows[0]['address'], 'username': userData.rows[0]['username'], 'public_key': userData.rows[0]['public_key']})
+        res.status(200).json({'address': userData.rows[0]['address'], 'username': userData.rows[0]['username'], 'public_key': userData.rows[0]['public_key'], 'messages':userMessages})
     }catch(err){
         console.error(err)
         req.error = {status:500, msg:'there was an error retreving your data'}
