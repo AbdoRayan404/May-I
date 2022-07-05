65c370b4a7d62152b498a19f7cf468482477cf6a
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-05 16:04:47 +0200

    [FEATURE] faster way to update user settings
    so instead of having two API endpoints for a user to update his store_messages option and public_key, why not making it in one route,
    that would make it easier for a front-end developer to interact with the backend faster, instead of the two endpoints.

--- user-ms/controllers/user/update.js
+++ user-ms/controllers/user/update.js
@@ -1,32 +1,46 @@
 const pool = require('../../model/database')
 
-async function updatePubKey(req, res, next){
-    let {address, public_key} = req.body;
-
-    //query payload
-    let query = { 
-        method: "UPDATE",
-        table: "users",
-        column: 'public_key',
-        columnValue: public_key,
-        conditionAddress: `address = '${address}'`,
-    }
-
-    //updating user public key
-    try{ 
-        const updatedData = await pool.query(`${query.method} ${query.table} SET ${query.column} = '${query.columnValue}' WHERE ${query.conditionAddress}`)
+async function updateSettings(req, res, next){
+    let {address, update} = req.body;
+    /*
+        expecting update Object to be
+            {
+                store_messages: Boolean,
+                public_key: String
+            }
+        //NOTE: it is optional, so a user might update public_key only
+    */
+    
+    if(typeof update.public_key == "string" && typeof update.store_messages == "boolean"){
+        const updatedData = await pool.query(`UPDATE users SET public_key = '${update.public_key}', store_messages = ${update.store_messages} WHERE address = '${address}'`)
         
         if(updatedData.rowCount == 1){
-            res.json({'address': address, 'public_key': public_key})
-        }else{
-            req.error = {status:500, msg:'your data didnt update successfuly'}
-            next()
+            res.status(200).json({"store_messages": update.store_messages, "public_key": update.public_key})
         }
-    }catch(err){
-        console.error(err)
-        req.error = {status:500, msg:'there was error updating your data'}
-        next()
+        return
     }
+
+    if(typeof update.store_messages == "boolean" && typeof update.public_key == "undefined"){
+        const updatedData = await pool.query(`UPDATE users SET store_messages = ${update.store_messages} WHERE address = '${address}'`)
+
+        if(updatedData.rowCount == 1){
+            res.status(200).json({"store_messages": update.store_messages})
+        }
+        return
+    }
+
+    if(typeof update.public_key == "string" && typeof update.store_messages == "undefined"){
+        const updatedData =  await pool.query(`UPDATE users SET public_key = '${update.public_key}' WHERE address = '${address}'`)
+
+        if(updatedData.rowCount == 1){
+            res.status(200).json({"public_key": update.public_key})
+        }
+        return
+    }
+
+    res.status(400).json({"error":"are you sure your input is correct?"})
+
+
 }
 
-module.exports = updatePubKey;
+module.exports = updateSettings;
--- user-ms/routes/user.js
+++ user-ms/routes/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 let register = require('../controllers/user/register')
 let login = require('../controllers/user/login')
-let updatePubKey = require('../controllers/user/update')
-let updateSettings = require('../controllers/user/updateSettings')
 let getUserMessages = require('../controllers/user/userMessages')
+let updateSettings = require('../controllers/user/update.js')
+
 
 //middlewares
 let inputCheck = require('../middlewares/handlers/inputHandler');
@@ -15,7 +15,6 @@ router
     .post('/register', inputCheck, register)
     .post('/login', credVerify, login)
     .post('/user/messages', credVerify, getUserMessages)
-    .put('/update/pubkey', credVerify, updatePubKey)
-    .put('/update/settings', credVerify, updateSettings)
+    .put('/user/settings', credVerify, updateSettings)
 
 module.exports = router
