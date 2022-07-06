ff50269e3cea4ba4b31a33f91f94559c4007c0b4
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-06 09:43:51 +0200

    [FIX] warpping update controller in try/catch

--- user-ms/controllers/user/update.js
+++ user-ms/controllers/user/update.js
@@ -8,34 +8,46 @@ async function updateSettings(req, res, next){
                 store_messages: Boolean,
                 public_key: String
             }
-        //NOTE: it is optional, so a user might update public_key only
+        //NOTE: it is optional, so a user might update public_key only or tore_messages only
     */
     
     if(typeof update.public_key == "string" && typeof update.store_messages == "boolean"){
-        const updatedData = await pool.query(`UPDATE users SET public_key = '${update.public_key}', store_messages = ${update.store_messages} WHERE address = '${address}'`)
+        try{
+            const updatedData = await pool.query(`UPDATE users SET public_key = '${update.public_key}', store_messages = ${update.store_messages} WHERE address = '${address}'`)
         
-        if(updatedData.rowCount == 1){
-            res.status(200).json({"store_messages": update.store_messages, "public_key": update.public_key})
+            if(updatedData.rowCount == 1){
+                res.status(200).json({"store_messages": update.store_messages, "public_key": update.public_key})
+            }
+            return
+        }catch(err){
+
         }
-        return
     }
 
     if(typeof update.store_messages == "boolean" && typeof update.public_key == "undefined"){
-        const updatedData = await pool.query(`UPDATE users SET store_messages = ${update.store_messages} WHERE address = '${address}'`)
+        try{
+            const updatedData = await pool.query(`UPDATE users SET store_messages = ${update.store_messages} WHERE address = '${address}'`)
 
-        if(updatedData.rowCount == 1){
-            res.status(200).json({"store_messages": update.store_messages})
+            if(updatedData.rowCount == 1){
+                res.status(200).json({"store_messages": update.store_messages})
+            }
+            return
+        }catch(err){
+
         }
-        return
     }
 
     if(typeof update.public_key == "string" && typeof update.store_messages == "undefined"){
-        const updatedData =  await pool.query(`UPDATE users SET public_key = '${update.public_key}' WHERE address = '${address}'`)
+        try{
+            const updatedData =  await pool.query(`UPDATE users SET public_key = '${update.public_key}' WHERE address = '${address}'`)
 
-        if(updatedData.rowCount == 1){
-            res.status(200).json({"public_key": update.public_key})
+            if(updatedData.rowCount == 1){
+                res.status(200).json({"public_key": update.public_key})
+            }
+            return
+        }catch(err){
+
         }
-        return
     }
 
     res.status(400).json({"error":"are you sure your input is correct?"})
