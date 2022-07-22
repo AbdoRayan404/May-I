b03630a001708fa7875fc25a047f212cb7fdf927
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-23 01:49:25 +0200

    update settings api endpoint

--- user-ms/controllers/user/update.js
+++ user-ms/controllers/user/update.js
@@ -2,57 +2,49 @@ const pool = require('../../model/database')
 
 async function updateSettings(req, res, next){
     let {address, update} = req.body;
-    /*
-        expecting update Object to be
-            {
-                store_messages: Boolean,
-                public_key: String
+    let updated = {address, updated:{}}
+
+    if(req.body.hasOwnProperty('update') == false){
+        res.status(400).json({'error':'provide settings field'})
+        return
+    }
+
+    try{
+        //update settings if entered
+        if(typeof update.settings.store_messages == 'boolean'){
+            const beforeData = await pool.query(`SELECT store_messages FROM settings WHERE address = '${address}'`)
+            if(beforeData.rowCount == 1) updated.updated['store_messages'] = {before: beforeData.rows[0]['store_messages']}
+
+            const updatedData = await pool.query(`UPDATE settings SET store_messages = ${update.settings.store_messages} WHERE address = '${address}'`)
+            if(updatedData.rowCount == 1) {
+                updated.updated['store_messages'].after = update.settings.store_messages
             }
-        //NOTE: it is optional, so a user might update public_key only or tore_messages only
-    */
-    
-    if(typeof update.public_key == "string" && typeof update.store_messages == "boolean"){
-        try{
-            const updatedData = await pool.query(`UPDATE users SET public_key = '${update.public_key}', store_messages = ${update.store_messages} WHERE address = '${address}'`)
+        }else{
+            res.status(400).json({'error':'make sure your types are correct'})
+            return
+        }
+
+        //update profile
+        const beforeData = await pool.query(`SELECT ${update.bio ? `bio` : ''}${update.profile_picture ? `, profile_picture` : ''}${update.username ? `, username` : ''}${update.public_key ? `, public_key` : ''} FROM profile WHERE address = '${address}'`)
+        Object.keys(beforeData.rows[0]).forEach((v,i)=>{
+            updated.updated[v] = {before: beforeData.rows[0][v]}
+        })
+
+
+        const updatedData = await pool.query(`UPDATE profile SET ${update.bio ? `bio = '${update.bio}' ` : ''}${update.profile_picture ? `, profile_picture = '${update.profile_picture}' ` : ''}${update.username ? `, username = '${update.username}'` : ''}${update.public_key ? `, public_key = '${update.public_key}'` : ''} WHERE address = '${address}'`)
+        if(updatedData.rowCount == 1){
+            Object.keys(updated.updated).forEach((v,i)=>{
+                if(v != 'store_messages') updated.updated[v].after = update[v]
+            })
+        }
         
-            if(updatedData.rowCount == 1){
-                res.status(200).json({"store_messages": update.store_messages, "public_key": update.public_key})
-            }
-            return
-        }catch(err){
-
-        }
+        res.status(200).json(updated)
+    }catch(err){
+        console.log(err)
+        req.error = {status:500, msg:'there was an error updating your profile'}
+        next()
     }
+}   
 
-    if(typeof update.store_messages == "boolean" && typeof update.public_key == "undefined"){
-        try{
-            const updatedData = await pool.query(`UPDATE users SET store_messages = ${update.store_messages} WHERE address = '${address}'`)
-
-            if(updatedData.rowCount == 1){
-                res.status(200).json({"store_messages": update.store_messages})
-            }
-            return
-        }catch(err){
-
-        }
-    }
-
-    if(typeof update.public_key == "string" && typeof update.store_messages == "undefined"){
-        try{
-            const updatedData =  await pool.query(`UPDATE users SET public_key = '${update.public_key}' WHERE address = '${address}'`)
-
-            if(updatedData.rowCount == 1){
-                res.status(200).json({"public_key": update.public_key})
-            }
-            return
-        }catch(err){
-
-        }
-    }
-
-    res.status(400).json({"error":"are you sure your input is correct?"})
-
-
-}
 
 module.exports = updateSettings;
