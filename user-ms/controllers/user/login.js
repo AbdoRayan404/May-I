04e6994c63cfa0f773e951c62552a322cd3e9a03
0xAPI48 <44875260+Terry-404@users.noreply.github.com>
2022-07-22 09:41:51 +0200

    [REF] updating login endpoint

--- user-ms/controllers/user/login.js
+++ user-ms/controllers/user/login.js
@@ -4,23 +4,30 @@ const {getAllMessages} = require('../../model/mongoController')
 async function login(req, res, next){
     let {address} = req.body;
 
-    let query = {
-        method: 'SELECT',
-        columns: `address, username, public_key, store_messages`,
-        table: 'users',
-        condition: 'WHERE',
-        conditionKey: 'address',
-        conditionValue: `'${address}'`
-    }
-
     try{
-        const userData = await pool.query(`${query.method} ${query.columns} FROM ${query.table} WHERE ${query.conditionKey} = ${query.conditionValue}`)
-        let userMessages = {}
-        if(userData.rows[0]['store_messages'] == true){
-            userMessages = await getAllMessages(address)
-        }
+        const profileData = await pool.query(`SELECT address, username, public_key, bio, profile_picture, joined_at FROM profile WHERE address = '${address}'`)
+        
+        if(profileData.rowCount == 1){
+            let profileDestruct = {...data} = profileData.rows[0]
 
-        res.status(200).json({'address': userData.rows[0]['address'], 'username': userData.rows[0]['username'], 'public_key': userData.rows[0]['public_key'], 'messages':userMessages})
+            const settingsData = await pool.query(`SELECT store_messages FROM settings WHERE address = '${address}'`)
+            const contactsData = await pool.query(`SELECT contact_address FROM contacts WHERE address = '${address}'`)
+
+            if(settingsData.rowCount == 1 && contactsData.rowCount >= 1){
+                profileDestruct.settings = {
+                    store_messages: settingsData.rows[0]['store_messages']
+                }
+
+                profileDestruct.contacts = []
+                contactsData.rows.forEach((v, i)=>{profileDestruct.contacts.push(v['contact_address'])})
+
+                if(settingsData.rows[0]['store_messages'] == true){
+                    profileDestruct.stored_messages = await getAllMessages(address)
+                    
+                }
+                res.status(200).json(profileDestruct)
+            }
+        }      
     }catch(err){
         console.error(err)
         req.error = {status:500, msg:'there was an error retreving your data'}
